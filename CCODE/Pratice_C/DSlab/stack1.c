d4509cb25a84f0afedc551609a3e10630c10f4fc
Zaid Shaikh <zaidparvez05@gmail.com>
2024-08-12 16:02:52 +0530

    added CCODE/Pratice_C/DSlab/stack1.c

---
+++ CCODE/Pratice_C/DSlab/stack1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+void push();
+void pop();
+void show();
+
+int const size = 5;
+int top = -1;
+int a[5];
+
+int main() {
+    int c;
+    while(1) {
+        printf("Press 1 to push,\npress 2 to pop,\nand 3 to show Stack,\npress 4 to stop\n");
+        scanf("%d", &c);
+        printf("\n");
+        switch(c) {
+            case 1:
+                push();
+            break;
+            case 2:
+                pop();
+            break;
+            case 3:
+                show();
+            break;
+            case 4:
+                return 0;
+            default:
+                printf("Invalid Input!\n");
+            break;
+        }
+    }
+    return 0;
+}
+
+void push() {
+    if (top == size - 1) {
+        printf("Stack overflow\n\n");
+    } else {
+        int value;
+        printf("Enter value to push: ");
+        scanf("%d", &value);
+        a[++top] = value;
+        printf("\n");
+    }
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack underflow\n\n");
+    } else {
+        printf("Popped value: %d\n", a[top--]);
+        printf("\n");
+    }
+}
+
+void show() {
+    if (top == -1) {
+        printf("Stack is empty\n\n");
+    } else {
+        printf("Stack elements:\n");
+        for (int i = 0; i <= top; i++) {
+            printf("%d ", a[i]);
+        }
+        printf("\n\n");
+    }
+}
