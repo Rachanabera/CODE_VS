7161368b4f8a6928eed1113fa341845d68f2d98e
Zaid Shaikh <zaidparvez05@gmail.com>
2024-08-15 04:50:34 +0530

    added CODEJAVA/PracticeCodes/addRecursion.java

---
+++ CODEJAVA/PracticeCodes/addRecursion.java
@@ -0,0 +1,21 @@
+package PracticeCodes;
+
+import java.util.Scanner;
+
+public class addRecursion {
+    public static int add(int n) {
+        if (n < 0) {
+            return 0;
+        } else {
+            return n + add(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int number = in.nextInt();
+            System.out.println("Result: " + add(number));
+        }
+    }
+}
