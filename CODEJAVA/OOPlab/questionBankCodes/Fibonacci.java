41e86f4dbbc52679169d9c61ebb844fdde931810
Zaid Shaikh <zaidparvez05@gmail.com>
2024-08-18 14:44:23 +0530

    added CODEJAVA/OOPlab

---
+++ CODEJAVA/OOPlab/questionBankCodes/Fibonacci.java
@@ -0,0 +1,29 @@
+package questionBankCodes;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter the number of elements for the Fibonacci series: ");
+            int n = in.nextInt();
+            
+            while(n <= 0) {
+                System.out.println("Please enter a positive integer.");
+                n = in.nextInt();
+            }
+
+            System.out.println("Fibonacci Series:");
+            for (int i = 0; i < n; i++) {
+                System.out.print(fibonacci(i) + " ");
+            }
+        }
+    }
+    
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
