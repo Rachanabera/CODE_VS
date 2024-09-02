e175e7b372ce35f74227e11f8e8ad347d48569fd
Zaid Shaikh <zaidparvez05@gmail.com>
2024-09-02 19:40:44 +0530

    added channel name

--- CODEJAVA/OOPlab/experiments/exp5.java
+++ CODEJAVA/OOPlab/experiments/exp5.java
@@ -1,6 +1,7 @@
 package experiments;
 
 import java.util.*;
+
 class exp5 {
 	public static void main(String[] args) {
 		try (Scanner in = new Scanner(System.in)) {
@@ -9,13 +10,13 @@ class exp5 {
 			double i1 = in.nextDouble();
 			complex A = new complex(r1, i1);
 			A.display('A');
-			
+
 			System.out.println("Enter real and imaginary parts of B:");
 			double r2 = in.nextDouble();
 			double i2 = in.nextDouble();
 			complex B = new complex(r2, i2);
 			B.display('B');
-			
+
 			complex C = new complex();
 			C.sum(A, B);
 		}
@@ -24,19 +25,23 @@ class exp5 {
 
 class complex {
 	double r, i;
+
 	complex() {
 		r = 0.0;
 		i = 0.0;
 	}
+
 	complex(double r, double i) {
 		this.r = r;
-		this.i  = i;
+		this.i = i;
 	}
+
 	void sum(complex A, complex B) {
-		System.out.println("Result = "+(A.r + B.r) +" + "+ (A.i + B.i) +"i");
+		System.out.println("Result = " + (A.r + B.r) + " + " + (A.i + B.i) + "i");
 	}
+
 	void display(char A) {
-		System.out.println(A +" = "+ r +" + "+ i +"i");
+		System.out.println(A + " = " + r + " + " + i + "i");
 		System.out.println();
 	}
 }
