ba87d4a3c4d2e193f48fcdce913c394546f6240e
Zaid Shaikh <zaidparvez05@gmail.com>
2024-09-19 01:43:32 +0530

    added channel name

--- CODEJAVA/OOPlab/questionBankCodes/forRecursion/countdigits.java
+++ CODEJAVA/OOPlab/questionBankCodes/forRecursion/countdigits.java
@@ -6,6 +6,11 @@ public class countdigits {
     static int c = 0;
 
     public static void count(int n) {
+        if(n == 0 && c == 0){
+            c++;
+            return;
+        }
+
         if (n == 0) {
             return;
         } else {
@@ -17,6 +22,7 @@ public class countdigits {
         try (Scanner in = new Scanner(System.in)) {
             System.out.print("Enter a number to count number of digits: ");
             int number = in.nextInt();
+            c = 0;
             count(number);
             System.out.println("Number of digits of " + number + " are: " + c);
         }
