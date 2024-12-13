a6fa263ab7895e8938d8e8a782767b0bb4eb1cf9
AwakenZombie42 <zaidparvez05@gmail.com>
2024-12-13 19:32:05 +0530

    Update Python

---
+++ Python/workSheet.py
@@ -0,0 +1,16 @@
+a = int(input("Enter a number: "));
+b = int(input("Enter another number: "));
+
+if a > b:
+    if (a - b) % 2 == 0:
+        print("The difference between", a, "and", b, "is even.");
+    else:
+        print("The difference between", a, "and", b, "is odd.");
+
+x = int(input("Enter a number: "));
+
+for i in range(10, 20):
+    if i == x:
+        print("In range");
+    else:
+        print("Out of range");
